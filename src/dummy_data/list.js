5b58d89e5442854d7b093cc4c5f0244fb1c64afd
PCMACHero <pcmacmen@gmail.com>
2018-11-02 17:56:01 -0700

    yay

---
+++ src/dummy_data/list.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: "go to store",
+        details: "Get eggs, bread, milk and cheese",
+        _id: '001'
+    },
+    {
+        title:'Wash car',
+        details:'Wax, and vacuum inside',
+        _id: '002'
+    },
+    {
+        title: 'Eat nachos',
+        details:'Dont forget the guac',
+        _id:'003'
+    },
+    {
+        title: 'Go to park wih family',
+        details: 'Go to the new park with the lake',
+        _id:'004'
+    }
+]
